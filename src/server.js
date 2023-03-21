1576d9de2a708d268c50d53c73bd7a079fd5bad4
nahyeongjin1 <skgudwls@konkuk.ac.kr>
2023-03-21 23:52:46 +0900

    Understanding router with rough code

--- src/server.js
+++ src/server.js
@@ -2,20 +2,29 @@ import express from "express";
 import morgan from "morgan";
 
 const PORT = 4000;
-const logger = morgan("dev");
 
 const app = express();
-
-const handleRoot = (req, res) => {
-  return res.send("<h1>It's not ready yet :(</h1>");
-};
-const handleLogin = (req, res) => {
-  return res.send("It's LOGIN PAGE :)");
-};
-
+const logger = morgan("dev");
 app.use(logger);
 
-app.get("/", handleRoot);
+const globalRouter = express.Router();
+
+const handleHome = (req, res) => res.send("Home");
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+
+const handleEditUser = (req, res) => res.send("Edit User");
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () =>
   console.log(`Server listening on http://localhost:${PORT}`);
