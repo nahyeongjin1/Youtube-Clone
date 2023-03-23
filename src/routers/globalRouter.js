8014ef6490c4cd504a2fde8e468f8101fd2cb59e
nahyeongjin1 <skgudwls@konkuk.ac.kr>
2023-03-23 20:55:40 +0900

    Planning routes

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { recommended } from "../controllers/videoControllers";
-import { join } from "../controllers/userControllers";
+import { recommended, search } from "../controllers/videoControllers";
+import { join, login } from "../controllers/userControllers";
 
 const globalRouter = express.Router();
 
 globalRouter.get("/", recommended);
 globalRouter.get("/join", join);
+globalRouter.get("/login", login);
+globalRouter.get("/search", search);
 
 export default globalRouter;
