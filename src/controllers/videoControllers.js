2d369a8defb55560b6fbeb783277629e0990f47b
nahyeongjin1 <skgudwls@konkuk.ac.kr>
2023-03-26 20:25:05 +0900

    Practicing interaction between array DB and html

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,33 +1,37 @@
+let videos = [
+  {
+    title: "First Video",
+    ratings: 5,
+    comments: 2,
+    createdAt: "20 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    ratings: 3,
+    comments: 2,
+    createdAt: "1 minutes ago",
+    views: 9,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    ratings: 4.3,
+    comments: 31,
+    createdAt: "43 minutes ago",
+    views: 452,
+    id: 3,
+  },
+];
 export const recommended = (req, res) => {
-  const videos = [
-    {
-      title: "First Video",
-      ratings: 5,
-      comments: 2,
-      createdAt: "20 minutes ago",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "Second Video",
-      ratings: 3,
-      comments: 2,
-      createdAt: "1 minutes ago",
-      views: 9,
-      id: 2,
-    },
-    {
-      title: "Third Video",
-      ratings: 4.3,
-      comments: 31,
-      createdAt: "43 minutes ago",
-      views: 452,
-      id: 3,
-    },
-  ];
   return res.render("home", { pageTitle: "Home", videos });
 };
-export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const see = (req, res) => {
+  const { id } = req.params; // === const id = req.params.id;
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `Watching ${video.title}` });
+};
 export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const search = (req, res) => res.send("Search Video");
 export const deleteVideo = (req, res) => res.send("Delete Video");
