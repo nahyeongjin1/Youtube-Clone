ee14f2ac9205466583bd539c136509b94453e4c4
nahyeongjin1 <skgudwls@konkuk.ac.kr>
2023-03-26 17:35:32 +0900

    Iterations and mixins in pug

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,10 +1,32 @@
-const fakeUser = {
-  username: "NHJ",
-  loggedIn: true,
+export const recommended = (req, res) => {
+  const videos = [
+    {
+      title: "First Video",
+      ratings: 5,
+      comments: 2,
+      createdAt: "20 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Second Video",
+      ratings: 3,
+      comments: 2,
+      createdAt: "1 minutes ago",
+      views: 9,
+      id: 2,
+    },
+    {
+      title: "Third Video",
+      ratings: 4.3,
+      comments: 31,
+      createdAt: "43 minutes ago",
+      views: 452,
+      id: 3,
+    },
+  ];
+  return res.render("home", { pageTitle: "Home", videos });
 };
-
-export const recommended = (req, res) =>
-  res.render("home", { pageTitle: "Home", fakeUser });
 export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
 export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const search = (req, res) => res.send("Search Video");
