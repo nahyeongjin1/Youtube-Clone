2b46a538f1660bd68ef3e5e08197bbdae8dafa7b
nahyeongjin1 <skgudwls@konkuk.ac.kr>
2023-03-31 23:28:46 +0900

    The more detailed the schema is, the better.

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -2,9 +2,9 @@ import Video from "../models/Video";
 
 export const home = async (req, res) => {
   const videos = await Video.find({});
-  return res.render("home", { pageTitle: "Home", videos: [] });
+  return res.render("home", { pageTitle: "Home", videos });
 };
-export const watch = (req, res) => {
+export const watch = async (req, res) => {
   const { id } = req.params; // === const id = req.params.id;
   return res.render("watch", { pageTitle: `Watching` });
 };
@@ -21,20 +21,28 @@ export const postEdit = (req, res) => {
 export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
-export const postUpload = (req, res) => {
+export const postUpload = async (req, res) => {
   const { title, description, hashtags } = req.body;
-  const video = new Video({
-    title,
-    description,
-    createdAt: Date.now(),
-    hashtags: hashtags
-      .split(",")
-      .map((word) => (word[0] === "#" ? word : `#${word}`)),
-    meta: {
-      views: 0,
-      rating: 0,
-    },
-  });
-  console.log(video);
-  return res.redirect("/");
+  try {
+    await Video.create({
+      title,
+      description,
+      createdAt: Date.now(),
+      hashtags: hashtags
+        .split(",")
+        .map((word) => word.trim())
+        .map((word) => (word.startsWith("#") ? word : `#${word}`)),
+      meta: {
+        views: 0,
+        rating: 0,
+      },
+    });
+    return res.redirect("/");
+  } catch (error) {
+    const errorMessage = error._message;
+    return res.render("upload", {
+      pageTitle: "Upload Video",
+      errorMessage,
+    });
+  }
 };
