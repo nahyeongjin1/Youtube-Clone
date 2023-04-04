284741dff17ea4e8ed5daf2ffbd05ad751d3a6f1
nahyeongjin1 <skgudwls@konkuk.ac.kr>
2023-04-04 19:46:09 +0900

    Make join page part 2

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,7 +1,16 @@
+import User from "../models/User";
+
 export const getJoin = (req, res) => res.render("join", { pageTitle: `Join` });
-export const postJoin = (req, res) => {
-  console.log(req.body);
-  res.end();
+export const postJoin = async (req, res) => {
+  const { name, email, username, password, location } = req.body;
+  await User.create({
+    name,
+    email,
+    username,
+    password,
+    location,
+  });
+  return res.redirect("/login");
 };
 export const login = (req, res) => res.send("Log in Page");
 export const see = (req, res) => res.send("See User's Profile");
