421c86c6aa2c119eab6cf6873f0ed99a46be7a46
nahyeongjin1 <skgudwls@konkuk.ac.kr>
2023-04-07 23:00:46 +0900

    Make the server aware that the user is logged in part 1

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -54,7 +54,8 @@ export const postLogin = async (req, res) => {
       errorMessage: `Wrong password :(`,
     });
   }
-  console.log("Log user in! coming soon :)");
+  req.session.loggedIn = true;
+  req.session.user = user;
   return res.redirect("/");
 };
 export const see = (req, res) => res.send("See User's Profile");
