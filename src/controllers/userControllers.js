97efaa207db25c124a13ada97d8e31cbc0be89ab
nahyeongjin1 <skgudwls@konkuk.ac.kr>
2023-04-10 21:00:28 +0900

    Github login part 2

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -58,6 +58,19 @@ export const postLogin = async (req, res) => {
   req.session.user = user;
   return res.redirect("/");
 };
+
+export const startGithubLogin = (req, res) => {
+  const baseUrl = "https://github.com/login/oauth/authorize";
+  const config = {
+    client_id: "d9dce10563f7eb545932",
+    allow_signup: false,
+    scope: "read:user user:email",
+  };
+  const params = new URLSearchParams(config).toString();
+  const finalUrl = `${baseUrl}?${params}`;
+  return res.redirect(finalUrl);
+};
+export const finishGithubLogin = (req, res) => {};
 export const see = (req, res) => res.send("See User's Profile");
 export const logout = (req, res) => res.send("Log Out");
 export const edit = (req, res) => res.send("Edit User");
