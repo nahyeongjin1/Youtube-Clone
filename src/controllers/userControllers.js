f9a93a26e04e0e08da776c74ee3e60f735a02a39
nahyeongjin1 <skgudwls@konkuk.ac.kr>
2023-04-15 13:57:07 +0900

    Implement log out

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -136,6 +136,8 @@ export const finishGithubLogin = async (req, res) => {
 };
 
 export const see = (req, res) => res.send("See User's Profile");
-export const logout = (req, res) => res.send("Log Out");
+export const logout = (req, res) => {
+  req.session.destroy();
+  return res.redirect("/");
+};
 export const edit = (req, res) => res.send("Edit User");
-export const remove = (req, res) => res.send("Remove User");
