0b338234e2d9fdc0e7e54ba540d9ae2fab203301
nahyeongjin1 <skgudwls@konkuk.ac.kr>
2023-05-03 23:11:47 +0900

    Implemented play and pause in video player

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,21 @@
-console.log("video player");
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volume = document.getElementById("volume");
+
+const handlePlayBtnClick = (e) => {
+  if (video.paused) video.play();
+  else video.pause();
+};
+
+const handleVideoPause = (e) => (playBtn.innerText = "Play");
+
+const handleVideoPlay = (e) => (playBtn.innerText = "Pause");
+
+const handleMute = (e) => {};
+
+playBtn.addEventListener("click", handlePlayBtnClick);
+video.addEventListener("pause", handleVideoPause);
+video.addEventListener("play", handleVideoPlay);
+muteBtn.addEventListener("click", handleMute);
