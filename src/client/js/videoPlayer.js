5d765dfc9cb13d831651617cbf9014ea34130c6f
nahyeongjin1 <skgudwls@konkuk.ac.kr>
2023-05-04 22:12:25 +0900

    Implemented mute and unmute in video player

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -2,20 +2,27 @@ const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
-const volume = document.getElementById("volume");
+const volumeRange = document.getElementById("volume");
 
 const handlePlayBtnClick = (e) => {
   if (video.paused) video.play();
   else video.pause();
+  playBtn.innerText = video.paused ? "Play" : "Pause";
 };
 
 const handleVideoPause = (e) => (playBtn.innerText = "Play");
 
 const handleVideoPlay = (e) => (playBtn.innerText = "Pause");
 
-const handleMute = (e) => {};
+const handleMute = (e) => {
+  if (video.muted) {
+    video.muted = false;
+  } else {
+    videomuted = true;
+  }
+  muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+  volumeRange.value = video.muted ? 0 : 0.5;
+};
 
 playBtn.addEventListener("click", handlePlayBtnClick);
-video.addEventListener("pause", handleVideoPause);
-video.addEventListener("play", handleVideoPlay);
 muteBtn.addEventListener("click", handleMute);
