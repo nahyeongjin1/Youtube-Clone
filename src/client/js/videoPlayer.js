5cda990c059024494e0dc0e37348658c156553a0
nahyeongjin1 <skgudwls@konkuk.ac.kr>
2023-05-07 21:41:22 +0900

    Decorating video player with scss

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,20 +1,26 @@
 const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
+const playBtnIcon = playBtn.querySelector("i");
 const muteBtn = document.getElementById("mute");
+const muteBtnIcon = muteBtn.querySelector("i");
 const volumeRange = document.getElementById("volume");
 const currentTime = document.getElementById("currentTime");
 const totalTime = document.getElementById("totalTime");
 const timeline = document.getElementById("timeline");
 const fullScreenBtn = document.getElementById("fullScreen");
+const fullScreenIcon = fullScreenBtn.querySelector("i");
 const videoContainer = document.getElementById("videoContainer");
+const videoControls = document.getElementById("videoControls");
 
+let controlsMovementTimeoutId = null;
+let controlsTimeoutId = null;
 let volumeValue = 0.5;
 video.volume = volumeValue;
 
 const handlePlayBtnClick = () => {
   if (video.paused) video.play();
   else video.pause();
-  playBtn.innerText = video.paused ? "Play" : "Pause";
+  playBtnIcon.classList = video.paused ? "fas fa-play" : "fas fa-pause";
 };
 
 const handleVideoPause = () => (playBtn.innerText = "Play");
@@ -28,7 +34,9 @@ const handleMute = () => {
   } else {
     video.muted = true;
   }
-  muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+  muteBtnIcon.classList = video.muted
+    ? "fas fa-volume-mute"
+    : "fas fa-volume-up";
   volumeRange.value = video.muted ? 0 : volumeValue;
 };
 
@@ -38,19 +46,19 @@ const handleVolumeInput = (event) => {
   } = event;
   if (video.muted) {
     video.muted = false;
-    muteBtn.innerText = "Mute";
+    muteBtnIcon.classList = "fas fa-volume-up";
   }
   volumeValue = value;
   video.volume = value;
 };
 
 const formatTime = (seconds) => {
-  return new Date(seconds * 1000).toISOString().substring(11, 19);
+  return new Date(seconds * 1000).toISOString().substring(14, 19);
 };
 
 const handleLoadedMetadata = () => {
   totalTime.innerText = formatTime(Math.floor(video.duration));
-  timeline.max = Math.floors(video.duration);
+  timeline.max = Math.floor(video.duration);
 };
 
 const handleTimeUpdate = () => {
@@ -69,17 +77,54 @@ const handleFullScreenBtnClick = () => {
   const fullscreenElement = document.fullscreenElement;
   if (fullscreenElement) {
     document.exitFullscreen();
-    fullScreenBtn.innerText = "Enter Full Screen";
+    fullScreenIcon.classList = "fas fa-expand";
   } else {
     videoContainer.requestFullscreen();
-    fullScreenBtn.innerText = "Exit Full Screen";
+    fullScreenIcon.classList = "fas fa-compress";
   }
+};
+
+const hideControls = () => videoControls.classList.remove("showing");
+
+const handleMouseMoveOnVideo = () => {
+  if (controlsTimeoutId) {
+    clearTimeout(controlsTimeoutId);
+    controlsTimeoutId = null;
+  }
+  if (controlsMovementTimeoutId) {
+    clearTimeout(controlsMovementTimeoutId);
+    controlsMovementTimeoutId = null;
+  }
+  videoControls.classList.add("showing");
+  controlsMovementTimeoutId = setTimeout(hideControls, 3000);
+};
+
+const handleMouseLeaveOnVideo = () => {
+  controlsTimeoutId = setTimeout(hideControls, 3000);
+};
+
+const handleVideoClick = () => {
+  handlePlayBtnClick();
+};
+
+const handleVideoDblclick = () => {
+  handleFullScreenBtnClick();
+};
+
+const handleSpaceBarDown = (event) => {
+  if (event.key == " " || event.key == "Spacebar" || event.key == 32)
+    handlePlayBtnClick();
 };
 
 playBtn.addEventListener("click", handlePlayBtnClick);
 muteBtn.addEventListener("click", handleMute);
 volumeRange.addEventListener("input", handleVolumeInput);
-video.addEventListener("loadedmetadata", handleLoadedMetadata);
+video.addEventListener("loadeddata", handleLoadedMetadata);
 video.addEventListener("timeupdate", handleTimeUpdate);
 timeline.addEventListener("change", handleTimelineChange);
 fullScreen.addEventListener("click", handleFullScreenBtnClick);
+videoContainer.addEventListener("mousemove", handleMouseMoveOnVideo);
+videoContainer.addEventListener("mouseleave", handleMouseLeaveOnVideo);
+video.addEventListener("click", handleVideoClick);
+video.addEventListener("dblclick", handleVideoDblclick);
+document.addEventListener("keydown", handleSpaceBarDown);
