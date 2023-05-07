d6f59b58e2dce7443fad0e4496035744530fd894
nahyeongjin1 <skgudwls@konkuk.ac.kr>
2023-05-07 21:58:43 +0900

    Register view controller

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -122,3 +122,14 @@ export const getDelete = async (req, res) => {
   user.save();
   return res.redirect("/");
 };
+
+export const registerView = async (req, res) => {
+  const { id } = req.params;
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.status(404);
+  }
+  video.meta.views = video.meta.views + 1;
+  await video.save();
+  return res.status(200);
+};
