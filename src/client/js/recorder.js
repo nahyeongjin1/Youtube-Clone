df50ffa3e7425e9fe0d801e1fc3a207575f7f7b7
nahyeongjin1 <skgudwls@konkuk.ac.kr>
2023-05-09 19:48:47 +0900

    Implemented preview of recording video

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,11 +1,13 @@
 const recordBtn = document.getElementById("recordBtn");
+const previewVideo = document.getElementById("preview");
 
 const handleRecordBtnClick = async () => {
   const stream = await navigator.mediaDevices.getUserMedia({
     audio: false,
     video: true,
   });
-  console.log(stream);
+  previewVideo.srcObject = stream;
+  previewVideo.play();
 };
 
 recordBtn.addEventListener("click", handleRecordBtnClick);
