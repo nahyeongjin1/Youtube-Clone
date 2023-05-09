36afb094bcd7b49cdc57a295072ee88025a41a0a
nahyeongjin1 <skgudwls@konkuk.ac.kr>
2023-05-09 20:07:42 +0900

    Implement recorder part 2 (Recording video)

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,13 +1,40 @@
 const recordBtn = document.getElementById("recordBtn");
 const previewVideo = document.getElementById("preview");
 
-const handleRecordBtnClick = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
+let stream;
+
+const handleStopRecording = () => {
+  recordBtn.innerText = "Start Recording";
+  recordBtn.removeEventListener("click", handleStopRecording);
+  recordBtn.addEventListener("click", handleRecordBtnClick);
+};
+
+const handleRecordBtnClick = () => {
+  recordBtn.innerText = "Stop Recording";
+  recordBtn.removeEventListener("click", handleRecordBtnClick);
+  recordBtn.addEventListener("click", handleStopRecording);
+
+  const recorder = new MediaRecorder(stream);
+  recorder.ondataavailable = (event) => {
+    console.log("recording done");
+    console.log(event);
+    console.log(event.data);
+  };
+  recorder.start();
+  setTimeout(() => {
+    recorder.stop();
+  }, 5000);
+};
+
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
     audio: false,
     video: true,
   });
   previewVideo.srcObject = stream;
   previewVideo.play();
 };
+
+init();
 
 recordBtn.addEventListener("click", handleRecordBtnClick);
