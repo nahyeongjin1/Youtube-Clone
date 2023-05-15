f90f38a351bf658b9014c5bff5a8642574730557
nahyeongjin1 <skgudwls@konkuk.ac.kr>
2023-05-15 23:04:39 +0900

    API route part 1

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,12 +1,19 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
-const textarea = form.querySelector("textarea");
-const btn = form.querySelector("form");
 
 const handleBtnClick = (event) => {
   event.preventDefault();
+  const textarea = form.querySelector("textarea");
   const comment = textarea.value;
   const videoId = videoContainer.dataset.id;
+  fetch(`/api/videos/${videoId}/comment`, {
+    method: "POST",
+    body: {
+      comment,
+    },
+  });
 };
 
-form.addEventListener("submit", handleBtnClick);
+if (form) {
+  form.addEventListener("submit", handleBtnClick);
+}
